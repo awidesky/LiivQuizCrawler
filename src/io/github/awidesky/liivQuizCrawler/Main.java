1d88f35297229403b16b55b9f59598cb85a165fd
Eugene Hong <awidesky@naver.com>
2024-08-06 18:56:44 +0900

    fix bug and add oneLiner

--- src/io/github/awidesky/liivQuizCrawler/Main.java
+++ src/io/github/awidesky/liivQuizCrawler/Main.java
@@ -1,25 +1,30 @@
 package io.github.awidesky.liivQuizCrawler;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Main {
 
 	private static boolean debug = false;
+	private static String[] oneLiner = new String[3];
 	
 	public static void main(String[] args) {
 		final String today = new SimpleDateFormat("M월 d일").format(new Date());
 		debug("Today : " + today);
-		String[] arr = find_quiz("쏠퀴즈(쏠야구) 정답 " + today, 7);
+		String[] arr = find_quiz("쏠퀴즈", 7);
 		for (int i = 1; i < arr.length; i++) {
 			if(arr[i].contains("퀴즈팡팡")) {
 				i++;
 				continue;
 			}
-			if (i % 2 == 0)
+			if (i % 2 == 0) {
 				System.out.println(arr[i]);
+				oneLiner[arr[i - 1].contains("출석퀴즈") ? 0 : 1] = arr[i];
+			}
 			else
 				System.out.print(arr[i] + " : ");
 		}
@@ -27,6 +32,10 @@ public class Main {
 		System.out.println();
 		arr = find_quiz("KB Pay 리브메이트 오늘의 퀴즈 정답 " + today.replace(" ", ""), 2);
 		System.out.println(arr[0] + " : " + arr[1]);
+		oneLiner[2] = arr[1];
+		
+		System.out.println("\n");
+		System.out.println(Arrays.stream(oneLiner).collect(Collectors.joining("    ")));
 	}
 	
 	static String[] find_quiz(String title, int n) {
