3e8dcab11b268b4bf134e36d0cca07b9c096cd87
Eugene Hong <awidesky@naver.com>
2025-07-17 10:57:32 +0900

    allow whitespace before <blockquote>

--- src/io/github/awidesky/liivQuizCrawler/Tstory.java
+++ src/io/github/awidesky/liivQuizCrawler/Tstory.java
@@ -82,7 +82,7 @@ public class Tstory {
 	}
 	private static String getQuizAnswer(String link) {
 		String[] html = HTML.getText(link);
-		Pattern titlePattern = Pattern.compile("<blockquote(.*?)>((<b>(\\s*)퀴즈\\s*정답(\\s*)</b>)|(<span(.*?)>(\\s*)퀴즈\\s*정답(\\s*)</span>))(.*?)</blockquote>");
+		Pattern titlePattern = Pattern.compile("^\\s*<blockquote(.*?)>((<b>(\\s*)퀴즈\\s*정답(\\s*)</b>)|(<span(.*?)>(\\s*)퀴즈\\s*정답(\\s*)</span>))(.*?)</blockquote>");
 		Pattern pattern = Pattern.compile("(<span(.*?)>(.+?)</span>)");
 		for(int i = 0; i < html.length; i++) {
 			Matcher tm = titlePattern.matcher(html[i]);
