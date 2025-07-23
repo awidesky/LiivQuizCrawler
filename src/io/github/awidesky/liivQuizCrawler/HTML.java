db6584e9a6abe8f821f154d3ef3788ab3c386c64
Eugene Hong <awidesky@naver.com>
2025-07-23 19:03:33 +0900

    use UTF-8 to read HTML

--- src/io/github/awidesky/liivQuizCrawler/HTML.java
+++ src/io/github/awidesky/liivQuizCrawler/HTML.java
@@ -4,49 +4,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.LinkedList;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class HTML {
+	
+	private static Charset htmlCharset = StandardCharsets.UTF_8;
+	
 	public static String[] getText(String url) {
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openConnection().getInputStream()))) {
-            String inputLine;
-            List<String> l = new LinkedList<>();
-            while ((inputLine = br.readLine()) != null) {
-                //System.out.println(inputLine);
-            	l.add(inputLine);
-            }
-            return l.toArray(String[]::new);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+		return openURLconnection(url, s -> s.toArray(String[]::new));
 	}
 	public static <T> List<T> getTextFilter(String url, Function<String, T> map) {
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openConnection().getInputStream()))) {
-			 return br.lines().map(map).filter(Objects::nonNull).toList();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+		return openURLconnection(url, s -> s.map(map).filter(Objects::nonNull).toList());
 	}
 	public static <T> Optional<T> getTextFilterFirstOptional(String url, Function<String, T> map) {
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openConnection().getInputStream()))) {
-			return br.lines().map(map).filter(Objects::nonNull).findFirst();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
+		return openURLconnection(url, s -> s.map(map).filter(Objects::nonNull).findFirst());
 	}
 	public static <T> T getTextFilterFirst(String url, Function<String, T> map) {
 		return getTextFilterFirstOptional(url, map).orElse(null);
 	}
 	public static String[] getTextUntilFind(String url, String str) {
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openConnection().getInputStream()))) {
-			return br.lines().takeWhile(s -> s.contains(str)).toArray(String[]::new);
+		return openURLconnection(url, l -> l.takeWhile(s -> s.contains(str)).toArray(String[]::new));
+	}
+	
+	private static <T> T openURLconnection(String url, Function<Stream<String>, T> f) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openConnection().getInputStream(), htmlCharset))) {
+			return f.apply(br.lines());
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
